f16dccad5a1e836171ca47a236b23d2604443557
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-09-20 11:37:42 +0300

    added query processing

--- Lab1/internal/api/server.go
+++ Lab1/internal/api/server.go
@@ -3,7 +3,7 @@ package api
 import (
 	"log"
 	"net/http"
-
+	"strings"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,12 +14,12 @@ type Service struct {
 }
 
 var services = []Service{
-	{"Владимиро-Суздальское", "", "image/image1.jpg"},
-	{"Галицко-Волынское", "", ""},
-	{"Киевское", "", ""},
-	{"Переяславское", "", ""},
-	{"Полоцкое", "", ""},
-	{"Черниговское", "", ""},
+	{"Владимиро-Суздальское", "", "../image/VS.png"},
+	{"Галицко-Волынское", "Галицко-Волынское княжество считалось основным опорным центром базы Древних Руссов", "../image/GV.jpeg"},
+	{"Киевское", "", "../image/K.svg"},
+	{"Переяславское", "", "../image/P.png"},
+	{"Полоцкое", "", "../image/Po.png"},
+	{"Черниговское", "", "../image/C.png"},
 }
 
 func StartServer() {
@@ -29,14 +29,42 @@ func StartServer() {
 
 	app.LoadHTMLGlob("../../templates/*.html")
 	app.Static("/image", "../../resources/image")
-	//app.Static("/css", "../../templates/css")
-	//app.Static("/font", "../../resources/font")
+	app.Static("/css", "../../templates/css/")
 
 	app.GET("/", func(c *gin.Context) {
+		title := strings.ToLower(c.Query("kingdom_name"))
+
+		if title == "" {
+			c.HTML(http.StatusOK, "index.html", gin.H{
+				"services": services,
+			})
+			return
+		}
+
+		sortedServices := []Service{}
+		for i := range services {
+			if strings.Contains(strings.ToLower(services[i].Name), title) {
+				sortedServices = append(sortedServices, services[i])
+			}
+		}
+
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"services": services,
-			"name":     services[0].Name,
+			"services": sortedServices,
 		})
+	})
+
+	app.GET("/:kingdom", func(c *gin.Context) {
+		title := c.Param("kingdom")
+		for _, service := range services {
+			if service.Name == title {
+				c.HTML(http.StatusOK, "kingdom.html", gin.H{
+					"Name":      		service.Name,
+					"PhotoPath":      	"../" + service.PhotoPath,
+					"Description":      service.Description,
+				})
+				return
+			}
+		}
 	})
 
 	app.Run(":8000")
