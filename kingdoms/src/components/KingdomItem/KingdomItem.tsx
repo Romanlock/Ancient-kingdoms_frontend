e34a8b803b18ae0e76c7f30b4b231af00767f5bf
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-10-21 10:11:59 +0300

    add btn listeners

--- kingdoms/src/components/KingdomItem/KingdomItem.tsx
+++ kingdoms/src/components/KingdomItem/KingdomItem.tsx
@@ -11,17 +11,39 @@ interface Props {
 }
 
 const KingdomItem: React.FC<Props> = ({kingdoms}) => {
-    return (
-        <div className="feed-kingdoms">
-          {kingdoms.map((kingdom, index) => (
-            <div className={`feed-kingdom feed-kingdom__kingdom${index}`} key={index}>
+  const kingdomAboutFunc = () => {
+    console.log('about');
+  }
+
+  const kingdomDeleteFunc = () => {
+    console.log('delete');
+  }
+
+  return (
+      <div className="feed-kingdoms">
+        {kingdoms.map((kingdom, index) => (
+          <div className={`feed-kingdom feed-kingdom__kingdom${index}`} key={index}>
+            <div className="feed-kingdom__kingdom__title">
               <h2>{kingdom.title}</h2>
+            </div>
+            <div className="feed-kingdom__kingdom__description">
               <p>{kingdom.description}</p>
+              </div>
+            <div className="feed-kingdom__kingdom__img">
               <img src={kingdom.img} alt={kingdom.title} />
             </div>
-          ))}
-        </div>
-      );
+            <div className="feed-kingdom__kingdom__btns">
+              <div className="feed-kingdom__kingdom__about-btn btn-primary-defautl">
+                <button onClick={kingdomAboutFunc}>Подробнее</button>
+              </div>
+              <div className="feed-kingdom__kingdom__delete-btn btn-primary-default">
+                <button onClick={kingdomDeleteFunc}>Удалить</button>
+              </div>
+            </div>
+          </div>
+        ))};
+      </div>
+    );
 }
 
 export default KingdomItem;
