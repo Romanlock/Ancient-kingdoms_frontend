14fed5a8eb648c07d468365a150cabb2d3053058
Roman Sulaymanov <92111647+Romanlock@users.noreply.github.com>
2023-10-21 06:02:01 +0300

    change base strucrure

---
+++ kingdoms/src/components/KingdomItem/KingdomItem.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface Kingdom {
+    title: string;
+    description: string;
+    img: string;
+}
+
+interface Props {
+    kingdoms: Kingdom[];
+}
+
+const KingdomItem: React.FC<Props> = ({kingdoms}) => {
+    return (
+        <div className="feed-kingdom">
+          {kingdoms.map((kingdom, index) => (
+            <div className={`feed-kingdom__kingdom${index}`} key={index}>
+              <h2>{kingdom.title}</h2>
+              <p>{kingdom.description}</p>
+              <img src={kingdom.img} alt={kingdom.title} />
+            </div>
+          ))}
+        </div>
+      );
+}
+
+export default KingdomItem;
--- kingdoms/src/main.tsx
+++ kingdoms/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import ITunesPage from './ITunesPage.tsx'
+import KingdomsFeed from './components/KingdomsFeed/KingdomsFeed'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ITunesPage />
+    <KingdomsFeed />
   </React.StrictMode>,
 )
