e83f54d52ca2b4910602c8819f0019fad82a5f50
Roman Sulaymanov <92111647+Romanlock@users.noreply.github.com>
2023-12-14 19:07:02 +0300

    Update KingdomPage.tsx

--- src/components/KingdomPage/KingdomPage.tsx
+++ src/components/KingdomPage/KingdomPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Container, Row, Image, Col } from 'react-bootstrap';
 import { KingdomsApi } from "../../utils/api/KingdomsApi/KingdomsApi";
 import { Kingdom } from "../../dataStrucrures/KingdomInterface";
 
@@ -26,40 +26,49 @@ const KingdomPage: React.FC = () => {
   }
 
   return (
-   <div className="kingdom-page">
-       <div className="kingdom" style={{ marginTop: '56px'}}>
+    <div className="kingdom-page">
+      <div className="kingdom" style={{ marginTop: '56px'}}>
           <div className="kingdom__title">
             <h2>{kingdom.Name}</h2>
           </div>
-
-          <Form>
-            <Form.Group className="mb-3" controlId="kingdomArea">
-              <Form.Label>Площадь</Form.Label>
-              <Form.Control type="text" disabled = {true} value={kingdom.Area} />
-            </Form.Group>
-          </Form>
-           <div className="kingdom__area">
-               <h2></h2>
-           </div>
-           <div className="kingdom__capital">
-               <h2>{kingdom.Capital}</h2>
-           </div>
-           <div className="kingdom__description">
-               <h2>{kingdom.Description}</h2>
-           </div>
-           <div className="kingdom__img">
-               <img src={kingdom.Image} alt={kingdom.Name} />
-           </div>
-           <div className="kingdom__state">
-               <h2>{kingdom.State}</h2>
-           </div>
+          <Container>
+            <Row style={{ justifyContent: 'center' }}>
+              <Col xs={4}>
+                <Image src={kingdom.Image} alt={kingdom.Name} style={{ width: '100%' }} rounded />
+              </Col>
+              <Col xs={4}>
+                <Form>
+                  <Form.Group className="mb-3" controlId="kingdomArea">
+                    <Form.Label>Площадь</Form.Label>
+                    <Form.Control type="text" disabled = {true} value={kingdom.Area} />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="kingdomCapital">
+                    <Form.Label>Столица</Form.Label>
+                    <Form.Control type="text" disabled = {true} value={kingdom.Capital} />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="kingdomDescription">
+                    <Form.Label>О княжестве</Form.Label>
+                    <Form.Control as="textarea" rows={3} type="text" disabled = {true} value={kingdom.Description} />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="kingdomState">
+                    <Form.Label>Статус</Form.Label>
+                    <Form.Control type="text" disabled = {true} value={kingdom.State} />
+                  </Form.Group>
+                  <Button 
+                    onClick={() => console.log('asdf')}>
+                      Захватить 
+                    </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="kingdom-page__btns">
+          <Link to="/kingdom">
+            <Button className="btn-primary kingdom-page__back-btn">На главную</Button>
+          </Link>
        </div>
-       <div className="kingdom-page__btns">
-           <Link to="/kingdom">
-               <Button className="btn-primary kingdom-page__back-btn">На главную</Button>
-           </Link>
-       </div>
-   </div>
+    </div>
    );
 }
 
